939a988a55c1649e83c9127c7df7eb41ace3cd87
Tenra <40557356+AppetiteTeam@users.noreply.github.com>
2023-10-24 06:58:29 +0600

    fix

--- lib/src/widgets/editor_tool_bar.dart
+++ lib/src/widgets/editor_tool_bar.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:rich_editor/src/utils/javascript_executor_base.dart';
 import 'package:rich_editor/src/widgets/check_dialog.dart';
 import 'package:rich_editor/src/widgets/fonts_dialog.dart';
-import 'package:rich_editor/src/widgets/insert_image_dialog.dart';
 import 'package:rich_editor/src/widgets/insert_link_dialog.dart';
 import 'package:rich_editor/src/widgets/tab_button.dart';
 
@@ -65,51 +64,6 @@ class EditorToolBar extends StatelessWidget {
                     if (link != null)
                       await javascriptExecutor.insertLink(link[0], link[1]);
                   },
-                ),
-                TabButton(
-                  tooltip: 'Insert Image',
-                  icon: Icons.image,
-                  onTap: () async {
-                    var link = await showDialog(
-                      context: context,
-                      builder: (_) {
-                        return InsertImageDialog();
-                      },
-                    );
-                    if (link != null) {
-                      if (getImageUrl != null && link[2]) {
-                        link[0] = await getImageUrl!(File(link[0]));
-                      }
-                      await javascriptExecutor.insertImage(
-                        link[0],
-                        alt: link[1],
-                      );
-                    }
-                  },
-                ),
-                Visibility(
-                  visible: enableVideo!,
-                  child: TabButton(
-                    tooltip: 'Insert video',
-                    icon: Icons.video_call_sharp,
-                    onTap: () async {
-                      var link = await showDialog(
-                        context: context,
-                        builder: (_) {
-                          return InsertImageDialog(isVideo: true);
-                        },
-                      );
-                      if (link != null) {
-                        if (getVideoUrl != null && link[2]) {
-                          link[0] = await getVideoUrl!(File(link[0]));
-                        }
-                        await javascriptExecutor.insertVideo(
-                          link[0],
-                          fromDevice: link[2],
-                        );
-                      }
-                    },
-                  ),
                 ),
                 TabButton(
                   tooltip: 'Underline',
